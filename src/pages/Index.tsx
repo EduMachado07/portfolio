8c2a89756afd0606afe726d60ae8872afa7b403d
eduardo <eduardo.silvamachado07@gmail.com>
2025-06-24 22:46:36 -0300

    feat(index): add layout pronto dos projetos com map de array

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,7 @@
 import imgPerfil from "@/assets/perfil.jpg";
+import { Button } from "@/components/ui/button";
 import {
+  ArrowRight,
   File,
   Github,
   Instagram,
@@ -9,6 +11,7 @@ import {
   Smartphone,
 } from "lucide-react";
 import type { ReactNode } from "react";
+import { Link } from "react-router-dom";
 
 interface IContact {
   title: string;
@@ -19,6 +22,20 @@ interface ISocialMedia {
   title: string;
   link: string;
   icon: ReactNode;
+}
+interface IProjects {
+  img: string;
+  title: string;
+  description: string;
+  linkProject: string;
+  type: (
+    | "Full Stack"
+    | "Frontend"
+    | "Backend"
+    | "Pessoal"
+    | "Escolar"
+    | "Cliente"
+  )[];
 }
 
 const socialMedia: ISocialMedia[] = [
@@ -40,13 +57,23 @@ const contact: IContact[] = [
   { title: "Email", label: "eduardo.silvamachado07@gmail.com", icon: <Mail /> },
   { title: "Celular", label: "(12) 98850-3575", icon: <Smartphone /> },
 ];
+const projects: IProjects[] = [
+  {
+    img: "",
+    title: "Fin X",
+    description: "descrição do projeto...",
+    linkProject: "/",
+    type: ["Full Stack", "Pessoal"],
+  },
+];
 
 const Index = () => {
   return (
-    <main>
-      <section className="flex dark:bg-[#222] rounded-lg overflow-hidden">
+    <main className="flex flex-col gap-8 pb-16">
+      {/* PRESENTATION */}
+      <section className="flex dark:bg-[#222] rounded-lg overflow-hidden shadow-lg dark:shadow-zinc-900">
         <section className="w-2/6">
-          <img src={imgPerfil} alt="" />
+          <img src={imgPerfil} alt="img perfil" />
         </section>
         <section className="w-4/6 p-8 flex flex-col gap-4">
           <p className="text-[#0D7DFF] font-semibold text-lg">
@@ -65,10 +92,9 @@ const Index = () => {
             desenvolvimento de soluções web.
             <br />
             <br />
-            Atualmente, estou em busca da minha
-            primeira oportunidade profissional na área, onde eu possa aplicar
-            meus conhecimentos, aprender com novos desafios e contribuir com
-            projetos de impacto.
+            Atualmente, estou em busca da minha primeira oportunidade
+            profissional na área, onde eu possa aplicar meus conhecimentos,
+            aprender com novos desafios e contribuir com projetos de impacto.
           </p>
           <hr />
 
@@ -99,7 +125,7 @@ const Index = () => {
                 {socialMedia.map((item, index) => (
                   <div
                     key={index}
-                    className="flex flex-col gap-2 text-zinc-50 border-2 rounded-lg py-2.5 px-4"
+                    className="flex flex-col gap-2 text-zinc-50 border-2 rounded-lg py-2.5 px-4 transition-transform duration-300 hover:-translate-y-1 hover:shadow-md hover:shadow-zinc-800"
                   >
                     <div className="flex items-end gap-2">
                       {item.icon}
@@ -114,10 +140,10 @@ const Index = () => {
             <div className="flex flex-col gap-4">
               <h1 className="text-zinc-50 font-semibold">Currículo</h1>
               <section className="flex gap-8">
-                <div className="flex flex-col gap-2 text-zinc-50 border-2 rounded-lg py-2.5 px-4">
+                <div className="flex flex-col gap-2 text-zinc-50 border-2 rounded-lg py-2.5 px-4 transition-transform duration-300 hover:-translate-y-1 hover:shadow-md hover:shadow-zinc-800">
                   <div className="flex items-end gap-2">
                     <File size={20} />
-                    <p className="font-semibold text-sm">Ver arquivo</p>
+                    <p className="font-semibold text-sm">Ver currículo</p>
                   </div>
                   {/* <a className="text-zinc-300">{item.label}</a> */}
                 </div>
@@ -125,6 +151,56 @@ const Index = () => {
             </div>
           </section>
         </section>
+      </section>
+
+      <h1 className="text-[#0D7DFF] font-bold text-2xl">Projetos</h1>
+
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        {projects.slice(0, 4).map((item, index) => (
+          <section
+            key={index}
+            className="w-full flex flex-col gap-2 dark:bg-[#222] rounded-lg overflow-hidden shadow-lg dark:shadow-zinc-900"
+          >
+            <div className="relative w-full h-72">
+              <img
+                src={item.img}
+                alt="Imagem do projeto"
+                className="w-full h-full object-cover"
+              />
+              <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent via-transparent to-zinc-950" />
+            </div>
+            <div className="flex flex-col gap-2 p-4">
+              <section className="flex gap-2 flex-wrap">
+                {item.type.map((tag, index) => (
+                  <div
+                    key={index}
+                    className="dark:text-white rounded-sm border-2 px-2 py-0.5 text-sm"
+                  >
+                    {tag}
+                  </div>
+                ))}
+              </section>
+              <h1 className="text-[#0D7DFF] font-bold text-2xl">
+                {item.title}
+              </h1>
+              <p className="dark:text-white font-bold text-base">
+                {item.description}
+              </p>
+              <div className="text-right">
+                <Link to={item.linkProject}>
+                  <Button
+                    size="sm"
+                    variant="link"
+                    className="group relative overflow-hidden gap-2"
+                  >
+                    Ver projeto
+                    <ArrowRight className="transition-transform duration-300 transform opacity-0 translate-x-1 group-hover:opacity-100 group-hover:translate-x-0" />
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          </section>
+        ))}
       </section>
     </main>
   );
